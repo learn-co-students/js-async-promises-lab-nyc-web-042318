83577b9335858d776b1e27477c6b2f60169e2192
github username <jeffreyhwang90@gmail.com>
2018-06-10 15:15:29 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  qContainer = document.getElementsByClassName('question-container')
+  qContainer[0].innerHTML = question.questionText
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+
+  return new Promise(function(resolve) {
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion() {
+  return new Promise(function(){
+    return document.querySelector('.question-container').innerHTML = ''
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return  document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach((btn) => btn.classList.toggle("hide"))
+}
+
+function displayQuestionOnClick() {
+  let ask = document.querySelector('.waves-effect')
+  ask.addEventListener('click', () => {
+    askQuestionThenRemoveQuestion(500)
+    toggleTrueAndFalseButtons()
+  })
+}
